aec8ef8ee79271d69a03f5429b4ecff63930b295
Jonathan Hartley <tartley@tartley.com>
2019-02-04 17:34:22 -0600

    toggle fullscreen now also cycles through screens

--- belter/window.py
+++ belter/window.py
@@ -14,6 +14,7 @@ class Window:
         self.win = pyglet.window.Window(
             caption="Belter", fullscreen=False, vsync=False
         )
+        # TODO extract to get_keys()
         self.keys = {
             key.F10: self.toggle_fullscreen,
         }
@@ -25,8 +26,21 @@ class Window:
     def get_fps_display(self):
         return pyglet.window.FPSDisplay(self.win)
 
+    def _get_next_screen(self):
+        current = self.win.screen
+        screens = self.win.display.get_screens()
+        for index, screen in enumerate(screens):
+            if screen == current:
+                next_index = (index + 1) % len(screens)
+                return screens[next_index]
+        else:
+            raise RuntimeError(f"Screen not found {current} in  {screens}")
+
     def toggle_fullscreen(self):
-        self.win.set_fullscreen(fullscreen=not self.win.fullscreen)
+        if self.win.fullscreen:
+            self.win.set_fullscreen(fullscreen=False)
+        else:
+            self.win.set_fullscreen(screen=self._get_next_screen())
 
     def clear(self):
         self.win.clear()
