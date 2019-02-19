2800df837b995a87e23b5ce54f58453038d31069
Jonathan Hartley <tartley@tartley.com>
2019-02-18 19:05:02 -0600

    test ship.shape

--- belter/tests/test_models.py
+++ belter/tests/test_models.py
@@ -1,6 +1,9 @@
+from py2d.Math import Polygon
+
 from ..models import Ship
 
 def test_Ship():
     actual = Ship()
-    assert isinstance(actual, Ship)
+    assert isinstance(actual.shape, Polygon)
+    assert len(actual.shape) == 3
 
