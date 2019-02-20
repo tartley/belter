e363572f3a8ada5f627112efad6720252414cecb
Jonathan Hartley <Jonathan.Hartley@ibm.com>
2019-02-19 21:50:38 -0600

    render subscribes to world.add_item

--- belter/render.py
+++ belter/render.py
@@ -1,8 +1,12 @@
 class Render:
 
-    def __init__(self, window):
+    def __init__(self, window, world):
         self.window = window
+        world.on_add_item.subscribe(self.on_add_item)
         self.fps_display = window.get_fps_display()
+
+    def on_add_item(self, item):
+        pass
 
     def on_draw(self):
         self.window.clear()
--- belter/tests/test_render.py
+++ belter/tests/test_render.py
@@ -1,16 +1,32 @@
 from unittest.mock import call, Mock
 
 from ..render import Render
+from ..world import World
 
-def test_render_constructor():
+def test_constructor():
     pygwin = Mock()
-    render = Render(pygwin)
+    render = Render(pygwin, World())
     assert render.window == pygwin
     assert render.fps_display == pygwin.get_fps_display.return_value
 
+def test_constructor_should_subscribe_on_add_item():
+    called = Mock()
+
+    class MyRender(Render):
+        def on_add_item(self, item):
+            called(item)
+
+    world = World()
+    render = MyRender(Mock(), world)
+    render.on_add_item = Mock()
+
+    world.add_item('item1')
+
+    assert called.call_args == call('item1')
+
 def test_on_draw():
     pygwin = Mock()
-    render = Render(pygwin)
+    render = Render(pygwin, Mock())
 
     render.on_draw()
 
