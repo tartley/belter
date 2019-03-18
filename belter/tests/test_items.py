99cf6107d5551769ac103f9777d3b4991e3bfa16
Jonathan Hartley <Jonathan.Hartley@ibm.com>
2019-03-17 21:34:30 -0500

    Items have an x,y position

--- belter/tests/test_items.py
+++ belter/tests/test_items.py
@@ -4,14 +4,18 @@ from colortuple import Color
 from ..items import Asteroid, Ship
 
 def test_Ship():
-    actual = Ship()
+    actual = Ship(1, 2)
     assert isinstance(actual.color, Color)
     assert isinstance(actual.shape, Polygon)
     assert len(actual.shape) == 3
+    assert actual.x == 1
+    assert actual.y == 2
 
 def test_Asteroid():
-    actual = Asteroid()
+    actual = Asteroid(3, 4)
     assert isinstance(actual.color, Color)
     assert isinstance(actual.shape, Polygon)
     assert len(actual.shape) == 3
+    assert actual.x == 3
+    assert actual.y == 4
 
