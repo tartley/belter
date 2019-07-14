df3d4a40a56a23ca4b64ce5d01c51803d5cfbf84
Jonathan Hartley <tartley@tartley.com>
2019-07-14 15:02:19 -0500

    rearrange initial item creation

--- belter/items.py
+++ belter/items.py
@@ -5,11 +5,14 @@ from py2d.Math import Polygon, Vector
 
 class Item:
 
-    def __init__(self, x, y):
+    COLOR = Color(50, 50, 50)
+
+    def __init__(self, x, y, dx=0, dy=0):
         self.x = x
         self.y = y
-        self.dx = (random.random() - 0.5) / 1000
-        self.dy = (random.random() - 0.5) / 1000
+        self.dx = dx
+        self.dy = dy
+        self.color = self.COLOR
 
     def update(self):
         self.x += self.dx
@@ -21,7 +24,7 @@ class Ship(Item):
         Vector(-0.5, -0.8),
         Vector(+0.5, -0.8),
     ])
-    color = Color(50, 100, 200)
+    COLOR = Color(50, 100, 200)
 
 class Asteroid(Item):
     shape = Polygon.from_pointlist([
@@ -29,8 +32,5 @@ class Asteroid(Item):
         Vector(-0.5, +0.6),
         Vector(+0.5, +0.6),
     ])
-    def __init__(self, *args):
-        super().__init__(*args)
-        light = random.randint(25, 100)
-        self.color = Color(light, light, light)
+    COLOR = Color(50, 150, 100)
 
--- belter/level.py
+++ belter/level.py
@@ -1,7 +1,13 @@
+from math import cos, pi, sin
+
 from .items import Asteroid, Ship
 
 def initial_items():
     yield Ship(0, 0)
-    for _ in range(20):
-        yield Asteroid(0, 0)
+    NUM = 24
+    RANGE = 0.75 # create ships around 0.75 of a full circle
+    theta = 0
+    for num in range(NUM):
+        yield Asteroid(sin(theta), cos(theta))
+        theta += pi * 2 / NUM * RANGE
 
